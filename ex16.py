d6c39fe1467cbb4515964847780753a00460e6d5
wangyuzong <wangyuzong@linxrobot.com>
2020-08-14 11:27:57 +0800

    Python first

---
+++ ex16.py
@@ -0,0 +1,34 @@
+from sys import argv
+
+scirpt, filename = argv
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit RETURN."
+
+raw_input("?")
+
+print "Opening the file..."
+target = open(filename, 'w')
+#target = open(filename)
+
+print "Truncating the file. Goodbye!"
+target.truncate()    #在追加模式下，要清除以前内容
+
+print "Now I'm going to ask you for three lines."
+
+line1 = raw_input("line 1: ")
+line2 = raw_input("line 2: ")
+line3 = raw_input("line 3: ")
+
+print "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print "And finally, we close it."
+target.close()
